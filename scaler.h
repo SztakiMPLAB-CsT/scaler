af6503099e9c7bbc005f998b0b3575a0dbd45041
SztakiMPLAB-CsT <49873572+SztakiMPLAB-CsT@users.noreply.github.com>
2020-05-02 14:19:53 +0200

    initial commit
    first working class, for usage

---
+++ scaler.h
@@ -0,0 +1,21 @@
+ class scaler
+{
+    public:
+    scaler() = delete;
+    scaler(int min, int max,int iterations){
+        int minsize = std::floor((max-min)/iterations);
+        for(int i= 0;i<iterations;i++)
+            container_.push_back(min+minsize);
+        for(int i= 0;i<(max-min)%iterations;i++)
+            container_[i]++;
+    }
+    typedef std::vector<int>::const_iterator const_iterator;
+    const_iterator begin() const{
+        return(container_.begin());
+    }
+    const_iterator end() const{
+        return(container_.end());
+    }
+    private:
+    std::vector<int> container_;
+};
